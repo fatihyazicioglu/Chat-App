83e824f1a95e02e75871ba2eea88e0da29c634a8
fatihyazicioglu <fy134388@gmail.com>
2022-12-13 17:22:20 +0100

    UserSchema Created

---
+++ backend/models/User.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const {isEmail} = require('validator');
+const bcrypt = require('bcrypt');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Can't be blank"]
+  },
+  email: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    required: [true, "Can't be blank"],
+    index: true,
+    validate: [isEmail, "invalid email"]
+  },
+  password: {
+    type: String,
+    required: [true, "Can't be blank"]
+  },
+  picture: {
+    type: String,
+  },
+  newMessages: {
+    type: Object,
+    default: {}
+  },
+  status: {
+    type: String,
+    default: 'online'
+  }
+}, {minimize: false});
+
+
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User
