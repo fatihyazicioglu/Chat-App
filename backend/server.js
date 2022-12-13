21e3356b369feba1e87c37375651890a47ca7410
fatihyazicioglu <fy134388@gmail.com>
2022-12-13 17:15:38 +0100

    mongodb Connected

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,17 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors());
 
+require('./connection')
+
 const server = require('http').createServer(app);
 const PORT = 5001;
 const io = require('socket.io')(server,{
     cors:{
-        origin
+        origin:"http://localhost:3000",
+        methods:["GET", "POST"]
     }
 })
 
+server.listen(PORT,()=>{
+    console.log('listening on port',PORT);
+})
