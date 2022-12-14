c07b13c803e94057ad0f94c2df5aa254c5be6842
fatihyazicioglu <fy134388@gmail.com>
2022-12-14 15:53:21 +0100

    react-redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import persistStore from "redux-persist/es/persistStore";
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
